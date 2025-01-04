0679bf44fae05e49d26fdb2a505afa162a6c1b41
sujon-ahmed-WD <sujon.mrist@gmail.com>
2025-01-04 16:54:27 +0600

    list_Capacity

--- stl_list.cpp
+++ stl_list.cpp
@@ -2,13 +2,22 @@
 using namespace std;
 int main()
 {
-    list<int> l={1,2,3,4,5};
-    int a[]={10,20,30};
-    list<int>l2(a,a+3);
-    // list<int>l2(l);
-    for(int val: l2)
+    // list<int> l={1,2,3,4,5};
+    // int a[]={10,20,30};
+    vector<int> v = {10, 20, 30};
+    // vector<int>l2(l);
+    // list<int>l2(a,a+3);
+    list<int> l2(v.begin(), v.end());
+    // l2.clear();
+    // if(l2.empty())
+    // {
+    //     cout<<"Empty";
+    // }
+    l2.resize(2);
+
+    for (int val : l2)
     {
-        cout<<val<<endl;
+        cout << val << endl;
     }
     return 0;
 }
